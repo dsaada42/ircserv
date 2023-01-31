22e75dc15e5dce7f357d471a3af6366141794dd8
dsaada <saada.dylan@gmail.com>
2023-01-31 20:06:24 +0100

    Cleaner: split server.cpp in 2 files
    all received commands treatment are now in serverCommands file

---
+++ srcs/serverCommands.cpp
@@ -0,0 +1,308 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   serverCommands.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dylan <dylan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 20:03:20 by dylan             #+#    #+#             */
+/*   Updated: 2023/01/31 20:04:36 by dylan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "server.hpp"
+
+//===========================================================================================
+//=                                                                                         =
+//=                                 RECEIVED COMMANDS                                       =
+//=                                                                                         =
+//===========================================================================================
+
+// ----- ADMIN -----
+void irc::server::ft_admin( irc::message * msg ){
+    if (msg->get_params().empty())
+        _messages.push(err::err_noadmininfo(msg->get_fd()));
+    else if (msg->get_params() != SERVER_NAME)
+        _messages.push(err::err_nosuchserver(msg->get_params(), msg->get_fd()));
+    else
+        _messages.push(err::err_noadmininfo(msg->get_fd()));
+}
+// ----- CAP -----
+void irc::server::ft_cap( irc::message * msg ){(void)msg;}//ignore CAP messages
+//=======================================================================================
+void irc::server::ft_info( irc::message * msg ){(void)msg;}
+void irc::server::ft_invite( irc::message * msg ){(void)msg;} //-> invite a user to a channel
+void irc::server::ft_join( irc::message * msg ){(void)msg;} //-> join a channel
+void irc::server::ft_kick( irc::message * msg ){(void)msg;} //-> operator kicks user from channel
+void irc::server::ft_kill( irc::message * msg ){(void)msg;} //-> operator kills user (delete user)
+void irc::server::ft_list( irc::message * msg ){
+    //lists all channels + topic if param is empty
+    // if params not empty , lists the channels listed with their topics (separated by comas)
+    // there is a space in the params, it must be the last param, and this param refers to a server
+    (void)msg;
+    //ERR_NOSUCHSERVER
+    //RPL_LISTSTART
+    //RPL_LIST
+    //RPL_LISTEND
+}
+//==============================================================================================
+// ----- MODE -----
+void irc::server::ft_mode(irc::message * msg){
+    irc::user   *current;
+    std::vector<str> args;
+    char        c;
+    
+    //channels ?
+    
+    //users    
+    args = ft_split(msg->get_params(), " ");
+    if (args.size() < 2){
+        _messages.push(err::err_needmoreparams(msg->get_cmd(), msg->get_fd()));
+        return;
+    }
+    current = find_user_by_nick(args[0]);
+    if (!current)
+        _messages.push(err::err_nosuchnick(args[0], msg->get_fd()));
+    else if (find_user_by_fd(msg->get_fd()) != current){
+        _messages.push(err::err_usersdontmatch(msg->get_fd()));
+    }
+    else{
+        if (args[1].size() == 2){
+            c = args[1].at(1);
+            if (c == 'i' || c == 'o' || c == 's'){
+                if (args[1].at(0) == '+'){
+                    if ( c == 'o' ) // ignore +o
+                        return;
+                    current->change_mode(c, true);
+                    _messages.push(rpl::rpl_umodeis(current->nickname(), current->mode(), current->fd()));
+                    return;
+                }        
+                else if (args[1].at(0) == '-'){
+                    current->change_mode(c, false);
+                    _messages.push(rpl::rpl_umodeis(current->nickname(), current->mode(), current->fd()));
+                    return;
+                }
+            }
+        }
+        else
+            _messages.push(err::err_umodeunknownflag(msg->get_fd()));
+    }
+}
+
+void irc::server::ft_names(irc::message * msg){
+    //lists all nicknames visible on all visible channels (if a channel is private or secret, it won't appear unless the user is part of it)
+    //params can be a list of channels (will get an answer only if channel is visible)
+    //RPL_NAMREPLY
+    //RPL_ENDOFNAMES 
+    (void)msg;
+}
+// ----- NICK -----
+void irc::server::ft_nick(irc::message * msg){
+    irc::user       *current;
+    
+    if (msg->get_params().empty())
+        _messages.push(err::err_nonicknamegiven(msg->get_fd()));
+    else if (check_nickname_rules(msg->get_params()) == false)
+        _messages.push(err::err_erroneusnickname(msg->get_params(), msg->get_fd()));
+    else{
+        current = find_user_by_fd(msg->get_fd());
+        if (is_a_nickname(msg->get_params()) == false){
+            if (current->connected())
+                _messages.push(cmd::cmd_nick(user_prefix(current), msg->get_params(), msg->get_fd()));
+            current->set_nickname(msg->get_params());
+        }
+        else if (current->nickname() != msg->get_params())
+            _messages.push(err::err_nicknameinuse(msg->get_params(), msg->get_fd()));
+    }
+}
+// ----- NOTICE -----
+void irc::server::ft_notice(irc::message * msg){(void)msg;}//(won't trigger any reply since we are a server)
+// ----- OPER -----
+void irc::server::ft_oper(irc::message * msg){
+    std::vector<str>    args;
+    irc::user           *current;
+    std::map<int, irc::user*>::iterator it;
+    
+    args = ft_split(msg->get_params(), " ");
+    if (args.size() < 2){
+        _messages.push(err::err_needmoreparams(msg->get_cmd(), msg->get_fd()));
+        return;
+    }
+    if ((current = find_user_by_nick(args[0])) != NULL){
+        if (current->fd() == msg->get_fd()){
+            if (args[1] == OPER_PASSWORD){
+                _messages.push(rpl::rpl_youreoper(current->nickname(), msg->get_fd()));
+                current->change_mode('o', true);
+                for (it = _users.begin(); it != _users.end(); it++){
+                    _messages.push(cmd::cmd_mode(current->nickname(), "+o", it->second->fd()));    
+                }
+            }
+            else{
+                _messages.push(err::err_passwdmissmatch(msg->get_fd()));
+            }
+        }
+    }
+    else{
+        _messages.push(err::err_nooperhost(msg->get_fd()));
+    }
+}
+// ----- PASS -----
+void irc::server::ft_pass(irc::message * msg){
+    irc::user * current;
+
+    if (msg->get_params().empty())
+        _messages.push(err::err_needmoreparams(msg->get_cmd(), msg->get_fd()));   
+    else{
+        current = find_user_by_fd(msg->get_fd());
+        if (current->connected()){
+            _messages.push(err::err_alreadyregistred(msg->get_fd()));
+        }
+        else{
+            if (msg->get_params() == _pass)
+                current->set_pass(true);
+            else
+                current->set_pass(false);
+        }
+    }
+}
+// ----- PART -----
+void irc::server::ft_part(irc::message * msg){
+    // quits channel (with optionnal message sent to current users in channel)
+    (void)msg;
+}
+// ----- PING -----
+void irc::server::ft_ping(irc::message * msg){(void)msg;}// server will ignore message but listen to the ping to refresh inactivity timestamp of sending user
+// ----- PONG -----
+void irc::server::ft_pong(irc::message * msg){
+    irc::user   *current;
+    
+    if (msg->get_params().empty()){
+        _messages.push(err::err_noorigin(msg->get_fd()));
+    }
+    else if (msg->get_params() != SERVER_NAME){
+        _messages.push(err::err_nosuchserver(msg->get_params(), msg->get_fd()));
+    }
+    else {
+        current = find_user_by_fd(msg->get_fd());
+        if (current)
+            current->set_ping(false);
+    }
+}
+// ----- PRIVMSG -----
+void irc::server::ft_privmsg(irc::message * msg){(void)msg;}
+// ----- QUIT -----
+void irc::server::ft_quit(irc::message * msg){
+    irc::user   *current;
+    std::map<int, irc::user*>::iterator it;
+
+    current = find_user_by_fd(msg->get_fd());
+    for (it = _users.begin(); it != _users.end(); it++){
+        if (it->second->nickname() != current->nickname())
+            _messages.push(cmd::cmd_quit(user_prefix(current), msg->get_trailing(), it->second->fd()));    
+    }
+    delete_user(current);
+}
+//----- STATS -----
+void irc::server::ft_stats(irc::message * msg){
+    std::map<int, irc::user*>::iterator it;
+    irc::user                           *current;
+    unsigned long                       uptime;
+
+    current = find_user_by_fd(msg->get_fd());
+    if (current){
+        if (msg->get_params() == "l"){
+            for (it = _users.begin(); it != _users.end(); it++){
+                _messages.push(rpl::rpl_statslinkinfo( current->nickname(), it->second->connection_stats(), msg->get_fd()));
+            }
+            _messages.push(rpl::rpl_endofstats(current->nickname(), "l", msg->get_fd()));    
+        }
+        else if (msg->get_params() == "u"){
+            uptime = get_time_ms() - _creation_time;
+            _messages.push(rpl::rpl_statsuptime(current->nickname(), ft_ms_to_duration(uptime), msg->get_fd()));
+            _messages.push(rpl::rpl_endofstats(current->nickname(), "u", msg->get_fd()));    
+        }
+    }
+    //STATS <query> <server>
+    //if server omitted , send RPL_ENDOFSTATS
+    //if server not ours, send ERR_NOSUCHSERVER
+    //we handle m (list of commands supported (number of time each command has been called))
+    //RPL_STATSCOMMANDS
+    //we handle u (uptime)
+    //RPL_STATSUPTIME
+    //we handle l (list of connections and stats on each connection)
+    //RPL_STATSLINKINFO
+    //RPL_ENDOFSTATS
+}
+//----- TIME -----
+void irc::server::ft_time(irc::message * msg){
+    if (!msg->get_params().empty() && msg->get_params() != SERVER_NAME){
+        _messages.push(err::err_nosuchserver(msg->get_params(), msg->get_fd()));
+    }
+    else
+        _messages.push(rpl::rpl_time(SERVER_NAME, msg->get_fd()));
+}
+// ----- TOPIC -----
+void irc::server::ft_topic(irc::message * msg){(void)msg;} //send topic of given channel 
+// ----- USER -----
+void irc::server::ft_user(irc::message * msg){
+    irc::user * current;
+    std::vector<str> args;
+
+    if (msg->get_params().empty() || msg->get_trailing().empty()){
+        _messages.push(err::err_needmoreparams(msg->get_cmd(), msg->get_fd()));   
+        return;
+    }
+    args = ft_split(msg->get_params(), " ");
+    if (args.size() < 3){
+        _messages.push(err::err_needmoreparams(msg->get_cmd(), msg->get_fd()));
+        return;
+    }
+    current = find_user_by_fd(msg->get_fd());
+    if (current->username().size() != 0){
+        _messages.push(err::err_alreadyregistred(msg->get_fd()));
+        return;
+    }
+    if (current){
+        current->set_username(args[0]);
+        current->set_fullname(msg->get_trailing());
+    }
+}
+// ----- VERSION -----
+void irc::server::ft_version(irc::message * msg){
+    irc::user *current;
+
+    current = find_user_by_fd(msg->get_fd());
+    if (!current)
+        return;
+    if (msg->get_params().empty() || msg->get_params() == SERVER_NAME)
+        _messages.push(rpl::rpl_version(current->nickname(), SERVER_VERSION, "", SERVER_NAME , "The one and only", msg->get_fd()));
+    else
+        _messages.push(err::err_nosuchserver(msg->get_params(), msg->get_fd()));
+}
+// ----- WHO ------
+void irc::server::ft_who(irc::message * msg){
+    std::vector<str> args;
+    irc::user        *current;
+    
+    if (msg->get_params().empty())
+        _messages.push(err::err_nonicknamegiven(msg->get_fd()));
+    else if ((current = find_user_by_fd(msg->get_fd())) != NULL){
+        args = ft_split(msg->get_params(), " ");
+        if (args.size() == 1){
+            //search what is given as param
+            //priority to search for channels, if name given isn't channel , search for everything
+        }
+        else if (args.size() == 2 && args[1] == "o"){
+            //search for param as operator
+        }
+        _messages.push(rpl::rpl_endofwho(current->nickname(), args[0], msg->get_fd()));
+    }
+}
+//============================================================
+void irc::server::ft_whois(irc::message * msg){(void)msg;}
+void irc::server::ft_whowas(irc::message * msg){(void)msg;}
+//=============================================================
+// ----- USERS -----
+void irc::server::ft_users(irc::message * msg){ _messages.push(err::err_usersdisabled(msg->get_fd())); }
+// ----- SUMMON -----
+void irc::server::ft_summon(irc::message * msg){ _messages.push(err::err_summondisabled(msg->get_fd())); }
